ff571b075f306d69917a22daa49533637d62456b
HristoRogue <aroquega@unsa.edu.pe>
2019-02-23 21:15:31 -0500

    question_app installed and database installed

--- myquestionbank/settings.py
+++ myquestionbank/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'question_app.apps.QuestionAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
