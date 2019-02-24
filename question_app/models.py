7690b8a3d37cc6e59a13a277d40566068705f313
HristoRogue <aroquega@unsa.edu.pe>
2019-02-23 21:14:30 -0500

    Question model created

--- question_app/models.py
+++ question_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=50)
+    answer = models.TextField(max_length=150)
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    use_rate = models.IntegerField()
+    def __str__(self):
+        return self.question_text
