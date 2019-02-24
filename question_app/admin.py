3965c90ad68a38be8a0d5be7b638dffad640dce6
HristoRogue <aroquega@unsa.edu.pe>
2019-02-24 10:59:56 -0500

    new models added to admin panel

--- question_app/admin.py
+++ question_app/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question,Test,GradedTest,TestQuestion,Theme,GradedTestquestion
+
 # Register your models here.
 admin.site.register(Question)
+admin.site.register(Test)
+admin.site.register(GradedTest)
+admin.site.register(TestQuestion)
+admin.site.register(GradedTestquestion)
+admin.site.register(Theme)
