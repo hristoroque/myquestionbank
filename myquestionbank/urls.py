9ade3d6fb0379b81ffc341e9226292bec73bd996
HristoRogue <aroquega@unsa.edu.pe>
2019-02-24 21:53:12 -0500

    question app url config added

--- myquestionbank/urls.py
+++ myquestionbank/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+
 urlpatterns = [
+    path('',include('question_app.urls')),
     path('admin/', admin.site.urls),
 ]
