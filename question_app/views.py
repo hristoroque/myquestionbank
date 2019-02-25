887f9e84f7fd99ba0570e3c9f46beab7dbc02820
HristoRogue <aroquega@unsa.edu.pe>
2019-02-24 21:54:49 -0500

    First views created

--- question_app/views.py
+++ question_app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpRequest,HttpResponseRedirect
+from django.contrib.auth import authenticate,logout
+from django.contrib.auth import login as login_auth
 
-# Create your views here.
+def loginpage(request):
+    return render(request,'question_app/login.html')
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request,username=username,password=password)
+    if user is not None:
+        login_auth(request,user)
+        return HttpResponseRedirect(reverse('question:main'))
+    else :
+        return HttpResponse("jajaj")
+
+def index(request):
+    return render(request,'question_app/index.html')
+
+def log_out(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('question:index'))
+    
+def main(request):
+    return render(request,'question_app/main.html',context = {'user': request.user})
