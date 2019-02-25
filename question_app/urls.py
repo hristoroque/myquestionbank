36bff49d65582026a6622da389b1b661fa49158d
HristoRogue <aroquega@unsa.edu.pe>
2019-02-24 21:54:32 -0500

    First url config

--- question_app/urls.py
+++ question_app/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 # Create your views here.
 
+app_name = "question"
 urlpatterns = [
-    path('login/',views.loginpage,name='login'),
-]
+    path('log/',views.loginpage,name='loginpage'),
+    path('login/',views.login,name="login"),
+    path('main/',views.main,name='main'),
+    path('logout/',views.log_out,name='logout'),
+    path('',views.index,name="index")
+]   
