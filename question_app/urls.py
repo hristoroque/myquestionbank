c73be33290ab7c15482fc53c9c44d46cab904182
HristoRogue <aroquega@unsa.edu.pe>
2019-02-25 21:30:50 -0500

    urls mapped

--- question_app/urls.py
+++ question_app/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
     path('login/',views.login,name="login"),
     path('main/',views.main,name='main'),
     path('logout/',views.log_out,name='logout'),
-    path('',views.index,name="index")
+    path('',views.index,name="index"),
+    path('main/questions',views.questions,name="questions"),
+    path('main/questions/creation/<int:theme_id>/',views.questioncreation,name="questioncreation"),
+    path('main/questions/theme_creation/',views.themecreation,name="themecreation"),
+    path('main/questions/theme_creation/create/',views.createtheme,name ="createtheme"),
+    path('main/questions/creation/<int:theme_id>/create/',views.createquestion,name='createquestion')
 ]   
