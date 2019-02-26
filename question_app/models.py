4a3511d04e51890bbb4c61c801183b7f04437822
HristoRogue <aroquega@unsa.edu.pe>
2019-02-25 21:30:11 -0500

    models revisited

--- question_app/models.py
+++ question_app/models.py
@@ -6,7 +6,7 @@ class Theme(models.Model):
     theme_description = models.TextField(max_length=150)
     user_id = models.ForeignKey(User,on_delete = models.CASCADE)
     def __str__(self):
-        self.theme_name
+        return self.theme_name
 
 class Question(models.Model):
     question_text = models.CharField(max_length=50)
