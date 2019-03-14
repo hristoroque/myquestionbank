b4c2cefec344f860efb057e1ce2175edc80ec23f
HristoRogue <aroquega@unsa.edu.pe>
2019-03-14 15:16:39 -0500

    <str:username> deleted and tests paths commmented

--- question_app/urls.py
+++ question_app/urls.py
@@ -4,26 +4,20 @@ from . import views
 
 app_name = "question"
 urlpatterns = [
-    path('log/',views.loginpage,name='loginpage'),
-    path('login/',views.login,name="login"),
-    path('<str:username>/',views.main,name='main'),
-    path('logout/',views.log_out,name='logout'),
-    path('',views.index,name="index"),
-    path('<str:username>/themes/',views.themes,name="themes"),
-    path('<str:username>/themes/<int:theme_pk>',views.theme,name="theme"),
-    path('<str:username>/themes/creation',views.theme_creation,name="theme_creation"),
-    path('<str:username>/themes/<int:theme_pk>/creation',views.question_creation,name="question_creation"),
-    path('<str:username>/themes/<int:theme_pk>/delete',views.delete_theme,name="delete_theme"),
-    path('<str:username>/themes/<int:theme_pk>/edit',views.theme_edit,name='theme_edit'),
-    path('<str:username>/themes/<int:theme_pk>/editing',views.edit_theme,name='edit_theme'),
-    path('<str:username>/tests/',views.tests,name="tests"),
+    path('',views.index,name='index'),
+    path('themes/',views.themes,name="themes"),
+    path('themes/<int:theme_pk>',views.theme,name="theme"),
+    path('themes/new',views.new_theme,name="new_theme"),
+    path('themes/<int:theme_pk>/edit',views.edit_theme,name="edit_theme"),
+    path('themes/<int:theme_pk>/delete',views.delete_theme,name="delete_theme"),
+    path('themes/<int:theme_pk>/new',views.new_question,name="new_question"),
+    path('themes/<int:theme_pk>/<int:question_pk>/edit',views.edit_question,name='edit_question'),
+    path('themes/<int:theme_pk>/<int:question_pk>/delete',views.delete_question,name='delete_question'),
+]   
+'''
+path('<str:username>/tests/',views.tests,name="tests"),
     path('<str:username>/tests/creation',views.test_creation,name="test_creation"),
     path('<str:username>/tests/create',views.create_test,name="create_test"),
-    path('<str:username>/themes/create',views.create_theme,name ="create_theme"),
-    path('<str:username>/themes/<int:theme_pk>/create',views.create_question,name='create_question'),
-    path('<str:username>/themes/<int:theme_pk>/<int:question_pk>/update',views.question_update,name='question_update'),
-    path('<str:username>/themes/<int:theme_pk>/<int:question_pk>/delete',views.delete_question,name='delete_question'),
-    path('<str:username>/themes/<int:theme_pk>/<int:question_pk>/updating',views.update_question,name='update_question'),
     path('<str:username>/tests/<int:test_pk>',views.test,name="test"),
     path('<str:username>/tests/<int:test_pk>/exam',views.test_taking,name="test_taking"),
     path('<str:username>/tests/<int:test_pk>/take',views.take_test,name="take_test"),
@@ -31,4 +25,4 @@ urlpatterns = [
     path('<str:username>/tests/review/<int:graded_test_pk>',views.test_review,name="test_review"),
     path('<str:username>/tests/review',views.test_performance,name='test_performance'),
     path('<str:username>/tests/<int:test_pk>/delete',views.delete_test,name='delete_test')
-]   
+    '''
